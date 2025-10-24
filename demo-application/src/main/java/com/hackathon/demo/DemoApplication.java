1d25bebd3dccb3443687d091ef1410c9e3676326
filipkuser <filipkuser2003@gmail.com>
2025-10-24 02:44:50 +0200

    feat: add DemoApplication impl

--- demo-application/src/main/java/com/hackathon/demo/DemoApplication.java
+++ demo-application/src/main/java/com/hackathon/demo/DemoApplication.java
@@ -1,4 +1,14 @@
 package com.hackathon.demo;
 
+import com.hackathon.sdk.annotation.EnableMonitoring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableMonitoring
 public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
